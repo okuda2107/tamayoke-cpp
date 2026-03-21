0e7f72a2331fb25a19cb00b6c55287b5478cc0ac
okuda <okuda2107@gmail.com>
2026-03-21 17:01:46 +0900

    wip: pose実装

--- tamayoke/actor/Pose.cpp
+++ tamayoke/actor/Pose.cpp
@@ -1,13 +1,39 @@
 #include "Pose.h"
 
+#include "input/pose/InputState.h"
+#include "input/pose/InputSystem.h"
 #include "renderer/RenderDB.h"
 
 Pose::Pose(ActorsSystem* system, PoseDeps& deps)
     : IActor(system, deps), db(deps.renderDB) {
-    // 線，円を初期化
-    db.AddLine(0, Vector3(100, 100, 0), Vector3(-100, -100, 0),
-               Vector3(1, 1, 1));
-    db.AddCircle(0, Vector3(0, 0, 0), 50, Vector3(1, 1, 1), true);
+    // 線
+    for (int i = 0; i < skelton_count; i++) {
+        db.AddLine(i, Vector3::Zero, Vector3::Zero, Vector3(1, 1, 1));
+    }
+    // 頭の円
+    db.AddCircle(0, Vector3::Zero, 100, Vector3(1, 1, 1), true);
+    // そのほかの円
+    for (int i = 1; i < joint_count; i++) {
+        db.AddCircle(i, Vector3::Zero, 10, Vector3(1, 1, 1), true);
+    }
 }
 
-void Pose::ActorInput(const pose::InputState& state) {}
+void Pose::ActorInput(const pose::InputState& state) {
+    // 線
+    for (int i = 0; i < skelton_count; i++) {
+        db.UpdateLine(i, [&state, i](ShapeLine& line) {
+            line.start = Vector3(state.keypoints[skelton[i][0]].x,
+                                 state.keypoints[skelton[i][0]].y, 0);
+            line.end = Vector3(state.keypoints[skelton[i][1]].x,
+                               state.keypoints[skelton[i][1]].y, 0);
+        });
+    }
+    // 円
+    for (int i = 0; i < joint_count; i++) {
+        db.UpdateCircle(i, [&state, i](ShapeCircle& circle) {
+            circle.Update(Vector3(state.keypoints[joint[i]].x,
+                                  state.keypoints[joint[i]].y, 0),
+                          circle.radius);
+        });
+    }
+}
--- tamayoke/actor/Pose.h
+++ tamayoke/actor/Pose.h
@@ -11,6 +11,28 @@ struct PoseDeps : ActorDeps {
 class Pose : public IActor<pose::InputState> {
     class RenderDB& db;
 
+    static constexpr int skelton_count = 12;
+    static constexpr int skelton[12][2] = {
+        {5, 6},  {5, 7},   {7, 9},   {6, 8},   {8, 10},  {5, 11},
+        {6, 12}, {11, 12}, {11, 13}, {13, 15}, {12, 14}, {14, 16},
+    };
+    static constexpr int joint_count = 13;
+    static constexpr int joint[13] = {
+        pose::InputState::KEYPOINT_NOSE,
+        pose::InputState::KEYPOINT_LEFT_SHOULDER,
+        pose::InputState::KEYPOINT_RIGHT_SHOULDER,
+        pose::InputState::KEYPOINT_LEFT_ELBOW,
+        pose::InputState::KEYPOINT_RIGHT_ELBOW,
+        pose::InputState::KEYPOINT_LEFT_WRIST,
+        pose::InputState::KEYPOINT_RIGHT_WRIST,
+        pose::InputState::KEYPOINT_LEFT_HIP,
+        pose::InputState::KEYPOINT_RIGHT_HIP,
+        pose::InputState::KEYPOINT_LEFT_KNEE,
+        pose::InputState::KEYPOINT_RIGHT_KNEE,
+        pose::InputState::KEYPOINT_LEFT_ANKLE,
+        pose::InputState::KEYPOINT_RIGHT_ANKLE,
+    };
+
    public:
     Pose(class ActorsSystem* system, PoseDeps& deps);
 
